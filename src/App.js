dc510c4de354c4c521ac002489026347c8574973
Andrzej Jabłoński <andrzej@audioszum.pl>
2019-08-31 15:05:00 +0200

    feat: the box editor component has been added, sass support

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.scss";
+
+function TimeBoxEditor() {
+  console.log("witaj");
+  return (
+    <div className="box-editor">
+      Witam
+      <label className="box-editor__label" htmlFor="">
+        Co robisz?
+        <input
+          className="box-editor__input"
+          value="Uczę się skrótów klawiaturowych"
+          type="text"
+        />
+      </label>
+      <label className="box-editor__label" htmlFor="">
+        Ile minut?
+        <input className="box-editor__input" value="25" type="number" />
+      </label>
+      <button className="box-editor__button">Zacznij</button>
+    </div>
+  );
+}
 
 function App() {
-  return (
-
-  );
+  return <TimeBoxEditor />;
 }
 
 export default App;
